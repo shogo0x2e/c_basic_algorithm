7766bfcbfae89da59386db782b95b0a028253c24
Shogo Kitada <shogo.kitada@outlook.jp>
2022-10-07 13:44:08 +0900

    first commit

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+ *	DATE:
+ *		13:39:15 金 10 月 07, 2022
+ *
+ *	AUTHOR:
+ *		Shogo Kitada (Twitter, GitHub: shogo0x2e)
+ *		Mail: shogo.kitada@outlook.jp
+ *
+ *	DESCRIPTION:
+ *		勉強スペース
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    
+    
+
+    return 0;
+}
